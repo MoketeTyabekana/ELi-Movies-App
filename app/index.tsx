1800920b2ea5d74bad93b1d111b726d360efe32d
Mokete <ematyabekana@gmail.com>
2025-09-24 21:45:30 +0200

    Updated the App metadata for the title and desccription

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,11 @@
 import OnboardingScreen from "@/components/OnboardingScreen";
 
 
+export const metadata = {
+  title: "ELi Movies App",
+  description: "Browse and favorite movies with detailed info powered by OMDb.",
+};
+
 export default function Index() {
   return <OnboardingScreen/>
 }
